dde9df4c462607207290c04916befae939515573
Lina Quintana <lquintana479@gmail.com>
2023-08-16 22:03:03 -0400

    added licenses and the sections in the generateMarkdown file

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // TODO: Include packages needed for this application
+const generateMarkdown=require("./utils/generateMarkdown");
+const fs=require("fs");
+const inquirer= require("inquirer");
 
 // WHEN I enter my project title
 // THEN this is displayed as the title of the README
@@ -14,13 +17,75 @@
 // THEN I am taken to the corresponding section of the README
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+{
+    type:"input",
+    message:"What is the project title?",
+    name:"title",
+},
+{
+    type:"editor",
+    message:"What is the project description?",
+    name:"description",
+},
+{
+    type:"",
+    message:"Table of Contents",
+    name:"description",
+},
+{
+    type:"",
+    message:"What is the project description?",
+    name:"installation",
+},
+{
+    type:"",
+    message:"What is the project description?",
+    name:"usage",
+},
+{
+    type:"list",
+    message:"Choose the following license:",
+    choices:["MIT", "Mozilla", "Apache", "No License"],
+    name: "license",
+},
+{
+    type:"",
+    message:"What is the project description?",
+    name:"contribution",
+},
+{
+    type:"",
+    message:"What is the project description?",
+    name:"test",
+},
+{
+    type:"input",
+    message:"What is your Github username?",
+    name:"username",
+},
+{
+    type:"input",
+    message:"What is your email?",
+    name:"email",
+},
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data), err=>{
+        if(err) throw err
+        console.log("success!")
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions) 
+    .then(data=>{
+        writeToFile("./output/README.md", data)
+    })
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,19 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license==="MIT"){
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+  }
+  else if(license==="Mozilla"){
+    return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
+  }
+  else if(license==="Apache"){
+    return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+  }
+  else if(license==="No License"){
+    return `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -8,13 +21,25 @@ function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license==="MIT"){
+    return `THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`
+  }
+  else if(license==="Mozilla"){
+    return `Covered Software is provided under this License on an “as is” basis, without warranty of any kind, either expressed, implied, or statutory, including, without limitation, warranties that the Covered Software is free of defects, merchantable, fit for a particular purpose or non-infringing. The entire risk as to the quality and performance of the Covered Software is with You. Should any Covered Software prove defective in any respect, You (not any Contributor) assume the cost of any necessary servicing, repair, or correction. This disclaimer of warranty constitutes an essential part of this License. No use of any Covered Software is authorized under this License except under this disclaimer.`
+  }
+else if(license==="Apache"){
+  return `Unless required by applicable law or agreed to in writing, Licensor provides the Work (and each Contributor provides its Contributions) on an “AS IS” BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied, including, without limitation, any warranties or conditions of TITLE, NON-INFRINGEMENT, MERCHANTABILITY, or FITNESS FOR A PARTICULAR PURPOSE. You are solely responsible for determining the appropriateness of using or redistributing the Work and assume any risks associated with Your exercise of permissions under this License.`
+}
+else if(license==="No License"){
+  return 
+}}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
   
-${renderLicenseBadge(data)}
+${renderLicenseBadge(data.license)}
 
   ## Description
 
